b84d334c6741cb6f6c9a9ca205c141f2d0478eb8
RndMnkIII <rndmnkiii@gmail.com>
2023-06-25 13:24:47 +0200

    initial commit

--- lesson01_top.v
+++ lesson01_top.v
@@ -2,12 +2,18 @@
 //Material del Workshop: "Como crear tu propio juego Arcade (y sobrevivir al intento)"
 module lesson01_top (
   input wire clk,
-  input wire reset,
-  output wire [3:0] video_r,
-  output wire [3:0] video_r,
-  output wire [3:0] video_r,
-  output wire csync
+  input wire reset
+  //output wire [3:0] video_r,
+  //output wire [3:0] video_g,
+  //output wire [3:0] video_b,
+  //output wire csync
 );
+  
+  //Vamos a crear un circuito con dos interruptores como entrada,
+  //Vamos a aplicar varias puertas logicas a estas entradas y ver que 
+  //salida producen
+  
+  
 
 endmodule
   
