37855614897953256bd21b4f43c7a8a02d6676a3
RndMnkIII <rndmnkiii@gmail.com>
2023-06-25 12:58:56 +0200

    Create lesson01_top.v

---
+++ lesson01_top.v
@@ -0,0 +1,13 @@
+//lesson01_top.v
+//Material del Workshop: "Como crear tu propio juego Arcade (y sobrevivir al intento)"
+module lesson01_top (
+  input wire clk,
+  input wire reset,
+  output wire [3:0] video_r,
+  output wire [3:0] video_r,
+  output wire [3:0] video_r,
+  output wire csync
+);
+
+endmodule
+  
